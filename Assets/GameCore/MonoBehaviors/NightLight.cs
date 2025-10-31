e9e1fceffe0674f54aa74774d4b5d8dbf3da3c84
PatrickStar_bro <15153686+patrickstarbro@user.noreply.gitee.com>
2025-10-31 21:07:57 +0800

    新增夜晚灯光类型

---
+++ Assets/GameCore/MonoBehaviors/NightLight.cs
@@ -0,0 +1,40 @@
+﻿/* 夜晚的灯光 By ShuaiGuo
+  2025年10月31日
+*/
+
+using GameCore.GlobalVars;
+using UnityEngine;
+using EventType = GameCore.Enum.EventType;
+
+namespace GameCore.MonoBehaviors
+{
+    public class NightLight : MonoBehaviour
+    {
+        #region UnityBehaviour
+
+        private void Start()
+        {
+            _GameObject = gameObject;
+            G.GameEventManager.AddEventListener(EventType.DayNightSwitch, _OnDayNightSwitch);
+            _GameObject.SetActive(false);
+        }
+
+        #endregion UnityBehaviour
+
+        #region PrivateMethods
+
+        private void _OnDayNightSwitch()
+        {
+            _GameObject.SetActive(!_GameObject.activeSelf);
+        }
+
+        #endregion PrivateMethods
+
+
+        #region Fields
+
+        private GameObject _GameObject;
+
+        #endregion Fields
+    }
+}
